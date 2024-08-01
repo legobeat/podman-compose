2c60a4fa98142d16976f041a07bb14ab371c6a66
legobt <6wbvkn0j@anonaddy.me>
2024-08-01 01:55:45 +0000

    wip: add envsubst

--- podman_compose.py
+++ podman_compose.py
@@ -32,13 +32,127 @@ try:
 except ImportError:
     from pipes import quote as cmd_quote  # pylint: disable=deprecated-module
 
-# import fnmatch
-# fnmatch.fnmatchcase(env, "*_HOST")
-
 import yaml
 from dotenv import dotenv_values
 
 __version__ = "1.2.0"
+
+############# BEGIN ENVSUSBST
+"""
+Substitute environment variables in a string.
+
+For more info:
+>>> from envsubst import envsubst
+>>> help(envsubst)
+"""
+# MIT License
+#
+# Copyright (c) 2019 Alex Shafer
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+_simple_re = re.compile(r'(?<!\\)\$([A-Za-z0-9_]+)')
+_extended_re = re.compile(r'(?<!\\)\$\{([A-Za-z0-9_]+)((:?-)([^}]+))?\}')
+
+
+def _repl_simple_env_var(env):
+    def f(m):
+        var_name = m.group(1)
+        return env.get(var_name, '')
+    return f
+
+
+def _repl_extended_env_var(env):
+    def g(m):
+        var_name = m.group(1)
+        default_spec = m.group(2)
+        if default_spec:
+            default = m.group(4)
+            default = _simple_re.sub(_repl_simple_env_var, default)
+            if m.group(3) == ':-':
+                # use default if var is unset or empty
+                env_var = env.get(var_name, None)
+                if env_var:
+                    return env_var
+                else:
+                    return default
+            elif m.group(3) == '-':
+                # use default if var is unset
+                return env.get(var_name, default)
+            else:
+                raise RuntimeError('unexpected string matched regex')
+        else:
+            return env.get(var_name, '')
+    return g
+
+
+def envsubst(string, env):
+    """
+    Substitute environment variables in the given string
+
+    The following forms are supported:
+
+    Simple variables - will use an empty string if the variable is unset
+      $FOO
+
+    Bracketed expressions
+      ${FOO}
+        identical to $FOO
+      ${FOO:-somestring}
+        uses "somestring" if $FOO is unset, or set and empty
+      ${FOO-somestring}
+        uses "somestring" only if $FOO is unset
+
+    :param str string: A string possibly containing environment variables
+    :return: The string with environment variable specs replaced with their values
+    """
+    # handle simple un-bracketed env vars like $FOO
+    a = _simple_re.sub(_repl_simple_env_var(env), string)
+
+    # handle bracketed env vars with optional default specification
+    b = _extended_re.sub(_repl_extended_env_var(env), a)
+    return b
+
+
+def main():
+    opened = False
+    f = sys.stdin
+    try:
+        try:
+            fn = sys.argv[1]
+            if fn != '-':
+                f = open(fn)
+                opened = True
+        except IndexError:
+            pass
+
+        data = f.read()
+        try:
+            data = data.decode('utf-8')
+        except AttributeError:
+            pass
+
+        sys.stdout.write(envsubst(data))
+    finally:
+        if opened:
+            f.close()
+############# END ENVSUBST
 
 script = os.path.realpath(sys.argv[0])
 
@@ -134,7 +248,7 @@ def strverscmp_lt(a, b):
 def parse_short_mount(compose, mount_str):
     # TODO: handle relative path according to spec. Will require some refactoring
     basedir = compose.dirname
-    mount_str = rec_subs(mount_str, compose.environ)
+    mount_str = envsubst(mount_str, compose.environ)
     mount_a = mount_str.split(":")
     mount_opt_dict = {}
     mount_opt = None
